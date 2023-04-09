d1711b5bd4ad580d490b124402796637854b3870
Pratham Bhoir <pratham.bhoir@buyceps.com>
2023-04-09 08:02:14 +0530

    issued_for update

--- backend/company/serializers.py
+++ backend/company/serializers.py
@@ -89,7 +89,8 @@ class RegisterComplaintSerializer(serializers.ModelSerializer):
         fields=['issued_by','issued_for','text','is_resolved', 'hr']
 
     def create(self, validated_data, employee):
-        employee2 = validated_data.pop('issued_for')
+        issued_for = validated_data.pop('issued_for')
+        employee2 = Employee.objects.get(user__phone_no = issued_for)
         hr = employee.hr
         complaint = Complaint.objects.create(issued_by = employee, issued_for=employee2, hr=hr, **validated_data)
         validated_data['issued_for'] = employee2.user.name
