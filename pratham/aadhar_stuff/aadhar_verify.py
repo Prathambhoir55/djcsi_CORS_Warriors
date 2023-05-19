2140299c1aa7ff4e2b072cb835440a6f11ea9e7f
Pratham Bhoir <pratham.bhoir@buyceps.com>
2023-05-19 11:48:10 +0530

    faces module

--- pratham/aadhar_stuff/aadhar_verify.py
+++ pratham/aadhar_stuff/aadhar_verify.py
@@ -3,7 +3,7 @@ from pyzbar.pyzbar import decode
 from pyaadhaar.utils import isSecureQr
 from pyaadhaar.decode import AadhaarSecureQr
 
-img = cv2.imread('example_qr5.jpeg')
+img = cv2.imread('example_qr3.jpeg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 code = decode(gray)
@@ -13,5 +13,7 @@ isSecureQR = (isSecureQr(qrData))
 
 if isSecureQR:
     secure_qr = AadhaarSecureQr(int(qrData))
+    print(secure_qr)
     decoded_secure_qr_data = secure_qr.decodeddata()
     print(decoded_secure_qr_data)
+    secure_qr.saveimage("filename.jpg")
